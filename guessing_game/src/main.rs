19a18d57537af038b3a7f58c8ef84fe5bd373e82
Greg Paton <gregpaton08@gmail.com>
2022-11-08 21:19:03 -0500

    adding comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,7 +13,8 @@ fn main() {
     // secret_number is of type i32, the default numeric type in Rust.
     // However, since we are comparing it to guess below, and the type of guess is u32, the
     // compiler will infer that this is also be u32 and set it to that type.
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng() // Create a random number generator that runs on this thread and that is seeded by the OS.
+        .gen_range(1..=100); // Generate a random number in the range [1, 100].
 
     // Creates and infinite loop.
     loop {
@@ -28,24 +29,29 @@ fn main() {
             // the return value is not being used and may contain an error.
             .expect("Failed to read line");
 
-        // Rust allows you to shadow the previous value of guess with a new one.
-        // The trim method removes leading and trailing whitespace.
-        // The parse method will parse to the specified type, in this case u32.
-        let guess: u32 = match guess.trim().parse() {
+        // Rust allows us to shadow the previous type/value of guess with a new one.
+        // Since this type is u32, and we later compare it to `guess`, Rust will infer that `guess`
+        // should _also_ be of type u32. Woah!
+        let guess: u32 = match guess
+        .trim()  // remove any leading or trailing whitespace
+        .parse() // parse the string to a different type, in this case u32
+        {
             Ok(num) => num,
-            // The underscore is a catch-all to match any error value.
-            Err(_) => continue,
+                        // The underscore is a catch-all to match any error value.
+        Err(_) => {
+            println!("Guess again, idiot!");
+            continue;
+        },
         };
 
-        // Print a formatted string. The {} is a placeholder for a variable value, provided after
-        // the string.
-        println!("You guessed: {}", guess);
+        // think of {} as little crab pincers that hold a value in place.
+        println!("You guessed {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You got it!");
                 break;
             }
         }
